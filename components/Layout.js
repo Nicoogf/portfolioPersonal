e6b5d07b9742a5d1714fffd319437a33cbef303f
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-01 10:28:23 -0300

    Estructura inicial del Portfolio

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,25 @@ import { Sora } from "@next/font/google" ;
 const sora = Sora({
   subsets: ['latin'] , 
   variable : '--font-sora' ,
-  weight: ['100' , '200' , '300']
-})
-const Layout = () => {
-  return <div>Layout</div>;
+  weight: ['100' , '200' , '300' , '400' , '500' , '600' , '700' , '800']
+}) ;
+
+//componentes
+import Nav  from "../components/Nav" ; 
+import Header  from "../components/Header" ; 
+import TopLeftImg from "../components/TopLeftImg" ;
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className= { `page bg-site text-white bg-cover  bg-no-repeat ${sora.variable} font-sora relative `}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}     
+    </div>
+  );
 };
 
 export default Layout;
