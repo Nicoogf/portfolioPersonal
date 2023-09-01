1153bf182c24deb42f9d4c067532b940455219c5
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-01 09:57:13 -0300

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,12 @@
+//Fuentes
+import { Sora } from "@next/font/google" ;
+
+//Configuracion de Fuentes 
+const sora = Sora({
+  subsets: ['latin'] , 
+  variable : '--font-sora' ,
+  weight: ['100' , '200' , '300']
+})
 const Layout = () => {
   return <div>Layout</div>;
 };
