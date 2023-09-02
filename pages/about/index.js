cc3a363f9d7f1bd3358b530223f38fec1f46b70d
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-02 16:47:09 -0300

    Av numero dos sobre el about

--- pages/about/index.js
+++ pages/about/index.js
@@ -125,32 +125,46 @@ const About = () => {
        <div className='container mx-auto h-full flex flex-col items-center xl:flex-row 
        gap-x-6'>
 
-        <div>Text</div>
+        <div className='flex-1 flex flex-col justify-center'>Text</div>
+        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
 
-        <div>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
-              { aboutData.map(( item , itemIndex )=> {
-                return (
+                { aboutData.map(( item , itemIndex )=> {
+                  return (
 
-                  <div 
-                    key={ itemIndex } 
-                    className={`${
-                      index === itemIndex && `text-accent after:w-[100%] after:bg-accent after:transition-all 
-                      after:duration-300`
-                      } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] 
-                        after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                    onClick={() => setIndex( itemIndex )}> 
-                  
-                    { item.title } 
+                    <div 
+                      key={ itemIndex } 
+                      className={`${
+                        index === itemIndex && `text-accent after:w-[100%] after:bg-accent after:transition-all 
+                        after:duration-300`
+                        } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] 
+                          after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                      onClick={() => setIndex( itemIndex )}> 
+                    
+                      { item.title } 
 
-                  </div>
-                );
-              })}
-             </div>
-           </div>  
+                    </div>
+                  );
+                })}
           </div>
-        </div>    
-       );            
-    }; 
+
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+              { aboutData[ index ].info.map(( item , itemIndex ) => {
+                return (
+                    <div key= { itemIndex }>
+
+                      {/* Title */}
+                      <div> { item.title } </div>
+                      <div className='hidden md:flex'> - </div> 
+                      <div > { item.stage } </div> 
+                      </div>
+                );          
+              })}
+         </div>
+        </div>
+      </div>
+    </div>    
+   );            
+  }; 
        
 export default About;
