6280c6cc08044825cf71b5e7b1c6093fbdeb0f16
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-04 15:23:01 -0300

    Av sobre la pagina testimonios parte 2

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -28,12 +28,12 @@ import { Swiper , SwiperSlide } from "swiper/react" ;
 
 //Import Estilos del Swipper
 import 'swiper/css' ;
-import 'swiper/css/free-mode' ;
+import 'swiper/css/navigation' ;
 import 'swiper/css/pagination' ;
 
 
 // Import required modules
-import { Pagination } from "swiper" ;
+import { Navigation, Pagination } from "swiper" ;
 
 // Iconos
 import { FaQuoteLeft } from "react-icons/fa" ;
@@ -45,52 +45,54 @@ import Image from "next/image" ;
 const TestimonialSlider = () => {
   return (
     <Swiper 
-      spaceBetween={ 10 }
+      navigation={ true }
       pagination = {{
         clickable: true 
         }}
-      modules={[ Pagination ]}
-      className="h-[280px] sm:h-[480px]">
+      modules={[ Navigation , Pagination ]}
+      className="h-[400px] sm:h-[480px]">
 
         { testimonialData.map(( person , index)=>{
             return(
               <SwiperSlide key={index}>
-                <div>
+                <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
 
                   { /* Avatar , Nombre , Posicion */ }
-                  <div> 
-                    <div>
+                  <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0"> 
+                    <div className="flex flex-col justify-center text-center">
+
                       {/* Avatar  */}
-                      <div> 
-                        Avatar Image 
+                      <div className="mb-2 mx-auto "> 
+                        <Image src={ person.image } width={100} height={100} alt=""></Image> 
                       </div>
 
                       {/* Nombre  */}
-                      <div> 
-                        Nombre
+                      <div className="text-lg"> 
+                        { person.name }
                       </div>
 
                       {/* Posicion  */}
-                      <div> 
-                        Posicion
+                      <div className="text-[12px] uppercase font-extralight tracking-widest"> 
+                      { person.position } 
                       </div>
                     </div>
 
                   </div>
 
                   {/* Quote y Mensaje */}
-                  <div className="bg-pink-500/10 flex-1 flex flex-col justify-center 
+                  <div className="flex-1 flex flex-col justify-center 
                   items-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 
                   xl:before:h-[200px] relative xl:pl-20">
 
                   {/* Quote icon*/}
-                  <div className="mb-4">                     
+                  <div className="mb-4"> 
+                    {/* Corregir icono 233*/}                    
                     <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto 
-                    md:mx-0" />
+                    md:mx-0"/>
                   </div>
 
                   { /* Mensajes */}
-                  <div className="xl:text-lg"> { person.message } </div>
+                  <div className="xl:text-lg text-center md:text-left"> { person.message } </div>
                   </div>
 
 
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,21 +1,38 @@
 //Componentes
 import TestimonialSlider from "../../components/TestimonialSlider" ;
 
+//Framer Motion
+import { motion } from "framer-motion" ;
+import { fadeIn } from "../../variants" ;
+
 const Testimonials = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center">
       <div className="container mx-auto h-full flex flex-col justify-center">
 
         {/* Titulo */}
-        <h2 className="h2 mb-8  xl:mb-0">
+        <motion.h2 
+          variants={fadeIn('up' , 0.2)}
+          initial='hidden'
+          animate= "show"
+          exit= "hidden"
+          className="h2 mb-8  xl:mb-0"
+        >
           What client <span className="text-accent"> say.</span> 
-         </h2>
+         </motion.h2>
 
          { /* Slider */}
 
-         <div>
+         <motion.div 
+            variants={fadeIn('up' , 0.4)}
+            initial='hidden'
+            animate= "show"
+            exit= "hidden"
+           >
+
           <TestimonialSlider />
-         </div>
+
+         </motion.div>
       </div>
     </div>
   ) ;
